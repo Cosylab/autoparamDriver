cf3662adbdbb01325957ec70beabedba1aa64833
Jure Varlec <jure.varlec@cosylab.com>
2021-12-08 10:47:58 +0100

    Import proposed driver, make it compile

---
+++ autoparamDriverSup/src/autoparamDriver.cpp
@@ -0,0 +1,94 @@
+#include "autoparamDriver.h"
+
+namespace Autoparam {
+
+Reason::Reason(char const *asynReason) {
+    // TODO
+}
+
+Reason::Reason(Reason const &other) {
+    *this = other;
+}
+
+Reason& Reason::operator=(Reason const &other) {
+    m_asynParamIndex = other.m_asynParamIndex;
+    m_function = other.m_function;
+    m_arguments = other.m_arguments;
+    return *this;
+}
+
+Reason::~Reason() {
+    // Nothing to do here.
+}
+
+std::string Reason::normalized() const {
+    // TODO
+    return std::string();
+}
+
+Driver::Driver(const char *portName, int interfaceMask, int interruptMask, int asynFlags, int autoConnect, int priority, int stackSize) :
+    asynPortDriver(portName, 1, interfaceMask, interruptMask, asynFlags, autoConnect, priority, stackSize)
+ {
+    // TODO
+}
+
+Driver::~Driver() {
+    for (ParamMap::iterator i = m_params.begin(), end = m_params.end();
+         i != end;
+         ++i) {
+        delete i->second;
+    }
+}
+
+asynStatus Driver::drvUserCreate(asynUser *pasynUser, const char *reason, const char **, size_t *) {
+    Reason parsed(reason);
+    std::string normalized = parsed.normalized();
+    asynParamType type;
+    try {
+        type = m_functionTypes.at(parsed.function());
+    } catch (std::out_of_range const&) {
+        asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR,
+                  "%s: port=%s no handler registered for '%s'",
+                  driverName, portName, parsed.function().c_str());
+        return asynError;
+    }
+
+    int index;
+    if (findParam(normalized.c_str(), &index) == asynSuccess) {
+        pasynUser->reason = index;
+    } else {
+        createParam(normalized.c_str(), type, &index);
+        m_params[index] = createReason(parsed);
+        pasynUser->reason = index;
+    }
+
+    return asynSuccess;
+}
+
+void Driver::handleResultStatus(asynUser *pasynUser, ResultBase const &result) {
+    pasynUser->alarmStatus = result.alarmStatus;
+    setParamAlarmStatus(pasynUser->reason, result.alarmStatus);
+    pasynUser->alarmSeverity = result.alarmSeverity;
+    setParamAlarmSeverity(pasynUser->reason, result.alarmSeverity);
+}
+
+Reason* Driver::reasonFromUser(asynUser *pasynUser) {
+    try {
+        return m_params.at(pasynUser->reason);
+    } catch (std::out_of_range const&) {
+        char const *paramName;
+        asynStatus status = getParamName(pasynUser->reason, &paramName);
+        if (status == asynSuccess) {
+            asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR,
+                      "%s: port=%s no handler registered for '%s'",
+                      driverName, portName, paramName);
+        } else {
+            asynPrint(this->pasynUserSelf, ASYN_TRACE_ERROR,
+                      "%s: port=%s no parameter exists at index %d",
+                      driverName, portName, pasynUser->reason);
+        }
+        return NULL;
+    }
+}
+
+}
---
+++ autoparamDriverSup/src/autoparamHandler.h
@@ -0,0 +1,141 @@
+#pragma once
+
+// Standard includes
+#include <string>
+#include <vector>
+
+// EPICS includes
+#include <asynPortDriver.h>
+#include <alarm.h>
+#include <epicsTypes.h>
+
+namespace Autoparam {
+
+class Reason {
+public:
+    explicit Reason(char const *asynReason);
+
+    Reason(Reason const &other);
+
+    Reason& operator=(Reason const &other);
+
+    // Najbrž bo derived driver rabil kaj več kot to, kar nudi Reason, tako da
+    // pričakujemo dedovanje
+    virtual ~Reason();
+
+    std::string const& function() const {
+        return m_function;
+    }
+
+    std::vector<std::string> const& arguments() const {
+        return m_arguments;
+    }
+
+    std::string normalized() const;
+
+private:
+    friend class Driver;
+
+    void setIndex(int index) {
+        m_asynParamIndex = index;
+    }
+
+    int index() const {
+        return m_asynParamIndex;
+    }
+
+    int m_asynParamIndex;
+    std::string m_function;
+    std::vector<std::string> m_arguments;
+};
+
+template <typename T>
+class Array {
+public:
+    Array() : m_data(NULL), m_size(0) {}
+    Array(T *value, size_t size) : m_data(value), m_size(size) {}
+    Array(std::vector<T> &vector) : m_data(vector.data()), m_size(vector.size) {}
+
+    T* data() const {
+        return m_data;
+    }
+
+    size_t size() const {
+        return m_size;
+    }
+
+private:
+    T *m_data;
+    size_t m_size;
+};
+
+struct ResultBase {
+    asynStatus status;
+    epicsAlarmCondition alarmStatus;
+    epicsAlarmSeverity alarmSeverity;
+
+    ResultBase() : status(asynStatus((-1))), alarmStatus(epicsAlarmCondition(-1)), alarmSeverity(epicsAlarmSeverity(-1)) {}
+};
+
+template <typename T = void>
+struct Result : ResultBase {
+    T value;
+
+    Result() : ResultBase(), value() {}
+};
+
+template <>
+struct Result<void> : ResultBase {};
+
+template <>
+struct Result<epicsUInt32> : ResultBase {
+    epicsUInt32 value;
+    epicsUInt32 mask;
+
+    Result() : value(), mask() {}
+};
+
+template <typename T>
+struct AsynType;
+
+template <typename T>
+struct Handlers {
+    typedef Result<void> WriteResult;
+    typedef Result<T> ReadResult;
+    typedef WriteResult (*WriteHandler) (Reason&, T);
+    typedef ReadResult (*ReadHandler) (Reason&);
+
+    static const asynParamType type = AsynType<T>::value;
+    WriteHandler writeHandler;
+    ReadHandler readHandler;
+
+    Handlers() : writeHandler(NULL), readHandler(NULL) {}
+};
+
+typedef Handlers<epicsInt32> Int32Handlers;
+template<> struct AsynType<epicsInt32> { static const asynParamType value = asynParamInt32; };
+typedef Handlers<epicsInt64> Int64Handlers;
+template<> struct AsynType<epicsInt64> { static const asynParamType value = asynParamInt64; };
+typedef Handlers<epicsUInt32> UInt32DigitalHandlers;
+template<> struct AsynType<epicsUInt32> { static const asynParamType value = asynParamUInt32Digital; };
+typedef Handlers<epicsFloat64> Float64Handlers;
+template<> struct AsynType<epicsFloat64> { static const asynParamType value = asynParamFloat64; };
+typedef Handlers<std::string> OctetHandlers;
+template<> struct AsynType<std::string> { static const asynParamType value = asynParamOctet; };
+
+typedef Handlers<Array<epicsInt8> > Int8ArrayHandlers;
+template<> struct AsynType<Array<epicsInt8> > { static const asynParamType value = asynParamInt8Array; };
+typedef Handlers<Array<epicsInt16> > Int16ArrayHandlers;
+template<> struct AsynType<Array<epicsInt16> > { static const asynParamType value = asynParamInt16Array; };
+typedef Handlers<Array<epicsInt32> > Int32ArrayHandlers;
+template<> struct AsynType<Array<epicsInt32> > { static const asynParamType value = asynParamInt32Array; };
+typedef Handlers<Array<epicsInt64> > Int64ArrayHandlers;
+template<> struct AsynType<Array<epicsInt64> > { static const asynParamType value = asynParamInt64Array; };
+typedef Handlers<Array<epicsFloat32> > Float32ArrayHandlers;
+template<> struct AsynType<Array<epicsFloat32> > { static const asynParamType value = asynParamFloat32Array; };
+typedef Handlers<Array<epicsFloat64> > Float64ArrayHandlers;
+template<> struct AsynType<Array<epicsFloat64> > { static const asynParamType value = asynParamFloat64Array; };
+
+// Ne verjamem, da se nam na tej točki splača zafrkavat z enumi in podobno. Bomo videli.
+
+}
